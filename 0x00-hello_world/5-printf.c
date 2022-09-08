22524a5fb8a29606e30ffeadb06ed268bd0e4bcf
Gasoremugwaneza <mugwanezagasore073@gmail.com>
2022-09-08 16:19:21 -0700

    question 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,31 @@
-#include<stdio.h>
-\**
-*main - "with proper grammar, but the outcome is a piece of art
-*return: 0 if exited properly, non zero otherwise 
-*/
-int main(void)
-{
-printf("\"with proper grammar, but the outcome is a piece of art,");
-return (0);
-}
+#include <stdio.h>                                                                                                                                       
+
+                                                                                                                                                         
+
+/**                                                                                                                                                      
+ *
+ *  *   main - "with proper grammar, but the outcome is a piece of art,                                                                                          
+ *
+ *   *    Return: 0 if exited properly, non zero otherwise                                                                                                   
+ *
+ *         */                                                                                                                                                 
+
+                                                                                                                                                         
+
+int main(void)                                                                                                                                           
+
+	                                                                                                                                                         
+
+{                                                                                                                                                        
+
+	                                                                                                                                                         
+
+	                puts("\"with proper grammar, but the outcome is a piece of art,");                                                                            
+
+			                                                                                                                                                         
+
+			                        return (0);                                                                                                                      
+
+						                                                                                                                                                         
+
+}   
