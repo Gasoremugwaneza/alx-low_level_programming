fafa24ba220c2c368fa9b4ff867d14a085fffd3a
Gasoremugwaneza <mugwanezagasore073@gmail.com>
2022-09-08 11:38:22 -0700

    question 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 int main()
 {
-  put = "Programming is like building a multilingual puzzle\n";
+  puts = "Programming is like building a multilingual puzzle";
 
   return(0);	 
 }  
