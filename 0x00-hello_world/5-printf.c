589c5cc0f0fcf3e2db3c9ed3e2de5f7f9915ec12
Gasoremugwaneza <mugwanezagasore073@gmail.com>
2022-09-08 15:52:57 -0700

    question 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,14 @@
 #include<stdio.h>
 
-/* this is printing using printf *\
+/**
+
+ * this is printing using printf
+ *and it is simple to use 
+ *\
  
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("\"with proper grammar, but the outcome is a piece of art,");
 return (0);
 
 }
