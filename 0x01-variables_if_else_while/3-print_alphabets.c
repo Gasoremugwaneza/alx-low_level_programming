d9d29506aac6629552e3896d63054587060d5ef2
Gasoremugwaneza <mugwanezagasore073@gmail.com>
2022-09-09 16:14:47 -0700

    question 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+
+
+
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+
+
+
+int main(void)
+
+{
+
+
+
+char love;
+
+
+
+for (love = 'a'; love <= 'z'; love++)
+
+putchar(love);
+
+for (love = 'A'; love <= 'Z'; love++)
+
+putchar(love);
+
+putchar('\n');
+
+return (0);
+
+}
+
+Footer
+
+© 2022 GitHub, Inc.
+
+Footer navigation
+
+Terms
+
+Privacy
