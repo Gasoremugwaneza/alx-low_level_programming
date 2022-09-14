19074b10615f68dadcb26377401ea80a80a35359
Gasoremugwaneza <mugwanezagasore073@gmail.com>
2022-09-14 09:56:56 -0700

    question 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+
+/**
+*islower - prints 1 or 0 depending on input
+*@c: first parameter
+*Description: prints all lowercase letters
+*Return: Always(0).
+*/
+
+
+
+int _islower(int c)
+
+{
+
+return (c >= 'a' && c <= 'z');
+
+}
