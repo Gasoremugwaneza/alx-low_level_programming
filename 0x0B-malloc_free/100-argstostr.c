d69afa3ed3002cf1d2a5bde627189138c6bd9885
Gasoremugwaneza <mugwanezagasore073@gmail.com>
2022-10-07 02:11:41 -0700

    question 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,51 +1,48 @@
-#include "main.h"
-
-#include <stdlib.h>
-
-#include <stdio.h>
-
+#include"main.h"
+#include<stdlib.h>
+#include<stdio.h>
 /**
-*argstostr - concatenates all the arguments of your program
-*@ac: number of arguments
-*@av: pointer to pointer to first element in array of pointers
-*Return: pointer to a new string, or NULL if it fail 
+*argstostr-concatenatesalltheargumentsofyourprogram
+*@ac:numberofarguments
+*@av:pointertopointertofirstelementinarrayofpointers
+*Return:pointertoanewstring,orNULLifitfail
 */
-char *argstostr(int ac, char **av)
+char*argstostr(intac,char**av)
 {
-char *a;
-int i, y, j, z, p;
-int len = 0;
-if (ac == 0 || av ==  NULL)
-return (NULL);
-for (i = 0; i < ac; i++)
+char*a;
+inti,y,j,z,p;
+intlen=0;
+if(ac==0||av==NULL)
+return(NULL);
+for(i=0;i<ac;i++)
 {
-for (y = 0; av[i][y] != '\0'; y++)
+for(y=0;av[i][y]!='\0';y++)
 len++;
 }
-a = malloc((len + (ac + 1)) * sizeof(char));
-if (a == NULL)
+a=malloc((len+(ac+1))*sizeof(char));
+if(a==NULL)
 {
-return (NULL);
+return(NULL);
 }
-j = 0;
-p = 0;
-z = 0;
-while (j < len + ac)
+j=0;
+p=0;
+z=0;
+while(j<len+ac)
 {
-if (av[p][z] != '\0')
+if(av[p][z]!='\0')
 {
-a[j] = av[p][z];
+a[j]=av[p][z];
 j++;
 z++;
 }
 else
 {
-a[j] = '\n';
+a[j]='\n';
 p++;
 j++;
-z = 0;
+z=0;
 }
 }
-a[j] = '\0';
-return (a);
+a[j]='\0';
+return(a);
 }
